ec61ec99e353fb5eca5cf64472d037284a122791
Bradley Dominguez <bradleydominguez25@gmail.com>
2017-03-01 13:29:35 +0000

    You've made changes

---
+++ script.js
@@ -0,0 +1,14 @@
+    TinyTurtle.apply(window); 
+    right(65);
+    forward(30);
+    right(50);
+    forward(30);
+    right(155);
+    forward(53);
+    left(90);
+    forward(24);
+    left(90);
+    forward(53);
+    left(90);
+    forward(24);
+    
